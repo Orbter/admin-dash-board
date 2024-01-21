40a58681402b51aecd7009fb14a048a18fbfb963
Orbter <assouline39@gmail.com>
2024-01-22 00:55:45 +0200

    finished most od the design of the pafe whats left is to do mobile version

---
+++ script.js
@@ -0,0 +1,26 @@
+const houMuch = document.querySelector(".hou-much");
+const theWinner = document.querySelector(".the-winner");
+document.addEventListener("DOMContentLoaded", function () {
+  if (houMuch && theWinner) {
+    houMuch.addEventListener("mouseover", function () {
+      theWinner.style.color = "var(--gold-color)";
+    });
+
+    houMuch.addEventListener("mouseout", function () {
+      theWinner.style.color = ""; // Revert to original color
+    });
+  }
+});
+theWinner.addEventListener("mouseover", function () {
+  houMuch.style.color = "var(--gold-color)";
+  houMuch.style.borderColor = "var(--gold-color)";
+  theWinner.style.color = "var(--gold-color)";
+  houMuch.style.boxShadow = "0px 0px 30px 10px rgba(252, 164, 0, 0.5)";
+});
+
+theWinner.addEventListener("mouseout", function () {
+  houMuch.style.color = "";
+  theWinner.style.color = "";
+  houMuch.style.borderColor = "";
+  houMuch.style.boxShadow = "";
+});
